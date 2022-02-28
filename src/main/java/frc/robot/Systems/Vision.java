5756132d65a8860257d6ccc3d12d72accc893a4e
Loki <71656278+underscoreL0K1@users.noreply.github.com>
2022-02-28 14:05:49 -0500

    Add files via upload

--- src/main/java/frc/robot/Systems/Vision.java
+++ src/main/java/frc/robot/Systems/Vision.java
@@ -5,20 +5,40 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 //this will hold the the commands for both the limelight and ball processing
 public class Vision {
 
-    public static double DistanceFromTarget() {
-        if (NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0) == 1) {return 0;}
-        else {
+    static double limLightAngle = 35.0; //PLACEHOLDER
+    static double limLightHeight = 36; //PLACEHOLDER
+    static double targetHeight = 72; //PLACEHOLDER
+    //all in inches
 
+
+    
+
+    public static double DistanceFromTarget() {
+        double vertDis = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0.0);
+        if (NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0) == 1) {     
+            double targetAngle = vertDis + limLightAngle;
+            double targetAngleRadians = targetAngle * (3.14159 / 180.0);
+            double tarDis = (targetHeight - limLightHeight)/Math.tan(targetAngleRadians);
+            return tarDis;
+        }
+        else {
             return 0;
         }
-
     }
 
-
     public static double AngleFromTarget() {
-        return 0;
+        double horDis = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0.0);
+        return horDis;
+    }
+
+    public static double AngleFromBall() {
+        double horDis = NetworkTableInstance.getDefault().getTable("SmartDashboard").getEntry("TargetX").getDouble(0.0);
+        return horDis;
     }
     
    
+
+
+
 
 }
